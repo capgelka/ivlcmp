8c865022d2d3e4cc9b161ed9611a05368e3e4f87
capgelka <ldyach@yandex.ru>
2015-01-03 03:55:51 +0300

    updated sillar portrait and compny image

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp -R The_undouing_of_Ivlentiy_the_Great Mine ~/.local/share/wesnoth/1.12/data/add-ons
+cp -R The_undouing_of_Ivlentiy_the_Great Mine ~/.local/share/wesnoth/1.12/data/add-ons; wesnoth
 
