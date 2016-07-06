b37c45460d9c6799d2e8c6bad9c4185ecb04a429
capgelka <ldyach@yandex.ru>
2016-07-06 23:34:05 +0300

    improved deploy sh to immediately run the game

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp -R The_undoing_of_Ivlentiy_the_Great factions_at_global_war ~/.local/share/wesnoth/1.12/data/add-ons; wesnoth
+cp -R The_undoing_of_Ivlentiy_the_Great factions_at_global_war ~/.local/share/wesnoth/1.12/data/add-ons;  wesnoth -c The_undoing_of_Ivlentiy_the_Great --campaign-difficulty 1 
 
